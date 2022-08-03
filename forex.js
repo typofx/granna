1134b0d634058e7d58683c7011d7b660d537b5dd
uarloque <uarloque@live.com>
2022-08-03 23:26:25 +0100

    Update forex.js

--- forex.js
+++ forex.js
@@ -1,4 +1,16 @@
-{
+<!DOCTYPE html>
+<html>
+<body>
+
+<form id="myForm">
+  <input id="BRLvalue" type="text" onfocusout="calcEUR()" onkeypress="enterKeyBRL()">
+  <input id="EURvalue" type="text" onfocusout="calcBRL()" onkeypress="enterKeyEUR()">
+</form>
+<br>
+<button onclick="calcEUR(),calcBRL()">Calc</button>
+
+
+<script>
     var forex;
 	const apiCryptoETH = 'https://api.coingecko.com/api/v3/coins/ethereum';
 async function getEURBRL()
@@ -7,37 +19,36 @@ async function getEURBRL()
 	const data = await response.json();
 	const { market_data } = data; 
     console.log(data);
-	//document.getElementById('ETHBRL').textContent = parseFloat(market_data.current_price.brl).toFixed(6);
-	//document.getElementById('ETHEUR').textContent = parseFloat(market_data.current_price.eur).toFixed(6);
-	//document.getElementById('EURBRLtext').textContent = parseFloat( (market_data.current_price.brl/market_data.current_price.eur) ).toFixed(6);
-	//document.getElementById('EURBRLtext2').textContent = parseFloat( (market_data.current_price.brl/market_data.current_price.eur) * (0.991) ).toFixed(6);
-	document.getElementById('EURBRLvalue').value = parseFloat( (market_data.current_price.brl/market_data.current_price.eur) * (0.991) ).toFixed(2);
+	document.getElementById('EURvalue').value = parseFloat( (market_data.current_price.brl/market_data.current_price.eur) * (0.989) ).toFixed(2);
 	document.getElementById('BRLvalue').value = parseFloat(1.00).toFixed(2);
-	forex = parseFloat( (market_data.current_price.brl/market_data.current_price.eur) * (0.991) ).toFixed(6);
+	forex = parseFloat( (market_data.current_price.brl/market_data.current_price.eur) * (0.989) ).toFixed(6);
 	}
     getEURBRL();
     
-}
-
-function sumBRL() { 
-		var txtFirstNo = parseFloat(document.getElementById('BRLvalue').value);
-		var result = parseFloat( txtFirstNo * forex ).toFixed(2);
-            if (!isNaN(result)) {
-                document.getElementById('EURBRLvalue').value = result;
-            }
-}
-
-function sumEUR() { 
-		var txtSecondNo = parseFloat(document.getElementById('EURBRLvalue').value);
-		var result = parseFloat( txtSecondNo / forex ).toFixed(2);
-            if (!isNaN(result)) {
-                //document.getElementById('result').value = result;
-                document.getElementById('BRLvalue').value = result;
-            }
-}
-       
 function round() { 
     document.getElementById('BRLvalue').value = parseFloat(document.getElementById('BRLvalue').value).toFixed(2);
-    document.getElementById('EURBRLvalue').value = parseFloat(document.getElementById('EURBRLvalue').value).toFixed(2);
-    
-}
+    document.getElementById('EURvalue').value = parseFloat(document.getElementById('EURvalue').value).toFixed(2);
+                 }
+
+    function enterKeyBRL() { if (event.keyCode === 13) calcEUR(); }
+    function enterKeyEUR() { if (event.keyCode === 13) calcBRL(); }
+
+
+function calcEUR() {
+        round();
+		var txtBRLvalue = parseFloat(document.getElementById('BRLvalue').value);
+		var result = parseFloat( txtBRLvalue * forex ).toFixed(2);
+            if (!isNaN(result)) document.getElementById('EURvalue').value = result;
+                  }
+
+function calcBRL() { 
+        round();
+		var txtEURvalue = parseFloat(document.getElementById('EURvalue').value);
+		var result = parseFloat( txtEURvalue / forex ).toFixed(2);
+            if (!isNaN(result)) document.getElementById('BRLvalue').value = result;
+                  }
+
+</script>
+
+</body>
+</html>
