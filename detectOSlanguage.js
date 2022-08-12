e9bf21808814cba260b77d761ef10355e4c14c87
Adam Soares <uarloque@live.com>
2022-08-12 22:21:41 +0100

    Update detectOSlanguage.js

--- detectOSlanguage.js
+++ detectOSlanguage.js
@@ -1,12 +1,12 @@
-<script>
-function detectLanguage() {
-    var phpadd = "<?php echo $whichLanguage = substr ($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2)?>";
-    alert(phpadd);
-}
-
-</script>
-
-<button onclick="detectLanguage()">Try it</button>
+// <script>
+// function detectLanguage() {
+//     var phpadd = "<?php echo $whichLanguage = substr ($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2)?>";
+//     alert(phpadd);
+// }
+// 
+// </script>
+// 
+// <button onclick="detectLanguage()">Try it</button>
 
 <?php
 
