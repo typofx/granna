b1a8d06663dfc24cef4baf5b36d427c683601270
Adam Soares <uarloque@live.com>
2022-08-19 23:36:59 +0100

    Update payeeTicket.php

--- payeeTicket.php
+++ payeeTicket.php
@@ -5,50 +5,58 @@ include 'LoginLanguages.php';
 include 'config.php';
 session_start();
 
-
+ $brlToSend = $_SESSION['brlToSend'];
+ $eurToReceive = $_SESSION['eurToReceive'];
+ $pix = $_SESSION['pix'];
 
 if (!isset($_SESSION['email']))
     {    header("Location: index.php");    }
 
-//$email = $_GET['email'];
-$email = $_SESSION['email'];
-
-// GMT ISSUE
-
-$dateRun = gmdate("H:i d/m/y");
-//$dateRun = gmdate("H,G,");
+    $email = $_SESSION['email'];
 
 $date = date_create();
-date_add($date,date_interval_create_from_date_string("2 days, 4 hours"));
-//echo date_format($date,"H:i d/m/y");
-$dateOver = date_format($date,"H:i d/m/y");
+ date_add($date,date_interval_create_from_date_string("6 hours"));
+ $dateRun = date_format($date,"H:i d/m/y");
 
+ date_add($date,date_interval_create_from_date_string("1 days,23 hours"));
+ $dateOver = date_format($date,"H:i d/m/y");
+
+    $sqlRecipient = "SELECT * FROM recipient WHERE ( (email='$email') AND ( pix = '$pix' ) )";
+	$resultRecipient = mysqli_query($conn, $sqlRecipient);
+		        if ($resultRecipient->num_rows > 0)
+		            {
+		                //echo "<script>alert('RECIPIENT THERE')</script>";
+		                $row = $resultRecipient->fetch_assoc();
+		                $namerecipient = $row["namerecipient"];
+		                //pix already here
+                        $bank = $row["bank"];
+                        $branch = $row["branch"];
+                        $account = $row["account"];
+                        $type = $row["type"];
+
+		            }
+		            else {echo "<script>alert('ERROR')</script>";}
 
 
 if (isset($_POST['submit']))
     {
-	//$email = $_POST['namerecipient'];
-	//$email = $_POST['email'];
 	$recipientID = $_SESSION['id'];
-	$pix = $_POST['pix'];
-	$brlToSend = $_POST['brlToSend'];
-	$eurToReceive = $_POST['eurToReceive'];
-	//$dateRun = $_POST['dateRun'];
-	//$dateOver = date_add($_POST['dateRun'],date_interval_create_from_date_string("2 days"));
+	$pix = $_SESSION['pix'];
+	$brlToSend = $_SESSION['brlToSend'];
+	$eurToReceive = $_SESSION['eurToReceive'];
 
-		//$sql = "SELECT * FROM recipient WHERE ( (email='$email') AND ( cpf='$cpf' OR pix = '$pix' ) )";
-		//$result = mysqli_query($conn, $sql);
-		//if (!$result->num_rows > 0)
-		   // {
-			$sql = "INSERT INTO ticket (email, recipientID, pix, brlToSend, eurToReceive, dateRun, dateOver)
-					VALUES ('$email', '$recipientID','$pix', '$brlToSend', '$eurToReceive', '$dateRun', '$dateOver')";
-			$result = mysqli_query($conn, $sql);
+		$sql = "INSERT INTO ticket (email, pix, brlToSend, eurToReceive, dateRun, dateOver)
+				VALUES ('$email', '$pix', '$brlToSend', '$eurToReceive', '$dateRun', '$dateOver')";
+		$result = mysqli_query($conn, $sql);
+            
+        
 
-			if ($result) {
-				//header('location: https://www.granna.ie/login/list.php?email='.$email);
-				//header('location: https://www.granna.ie/login/main.php');
-			   } else {         echo "<script>alert('OKAY')</script>";}
-		               //} else { echo "<script>alert('Woops! Recipient Already Exists.')</script>";}
+			if ($result) { 
+
+			    //echo "<script>alert('OKAY')</script>";
+                    header("location: https://www.granna.ie/login/selectBank.php");
+				
+			   } else { echo "<script>alert('FAIL')</script>";}
 	}
 
 ?>
@@ -70,17 +78,26 @@ if (isset($_POST['submit']))
 	<div class="container">
 		<form action="" method="POST" class="login-email">
             <p class="login-text" style="font-size: 2rem; font-weight: 800;"><center><h2>New Ticket</h2></center><br></p>
-
-			<div class="input-group"><input type="text" placeholder="$_SESSION['email']" name="email" value="<?php echo $_SESSION['email']; ?>" ></div>
-			<div class="input-group"><input type="text" placeholder="$_SESSION['id'];" name="recipientID"  value="<?php echo $_SESSION['id']; ?>" ></div>
-			<div class="input-group"><input type="text" placeholder="pix" name="pix" value="<?php echo $pix; ?>" ></div>
-			<div class="input-group"><input type="text" placeholder="brlToSend" name="brlToSend" value="<?php echo $brlToSend; ?>" ></div>
-			<div class="input-group"><input type="text" placeholder="eurToReceive" name="eurToReceive" value="<?php echo $eurToReceive; ?>" ></div>
-			<div class="input-group"><input type="text" placeholder="dateRun" name="dateRun" value="<?php echo $dateRun?>" disabled></div>
-			<div class="input-group"><input type="text" placeholder="dateOver" name="dateOver" value="<?php echo $dateOver?>" disabled></div>
-
-			<div class="input-group"><button name="submit" class="btn">Add Ticket</button></div>
+            
+            <div class="input-group"><label for="BRLvalue">Registered email</label>      <input type="text" name="email" value="<?php echo $_SESSION['email']; ?>" disabled>   </div>
+			<div class="input-group"><label for="BRLvalue">Payee's PIX Key</label>             <input type="text" name="pix" value="<?php echo $pix; ?>" disabled>                   </div>
+			<div class="input-group"><label for="BRLvalue">EUR to be transfered</label>  <input type="text" name="eurToReceive" value="<?php echo $eurToReceive; ?>" disabled> </div>
+			<div class="input-group"><label for="BRLvalue">BRL to be received</label>    <input type="text" name="brlToSend" value="<?php echo $brlToSend; ?>" disabled>       </div> 
+			<div class="input-group"><label for="BRLvalue">Order created at</label>      <input type="text" name="dateRun" value="<?php echo $dateRun?>" disabled>             </div>
+			<div class="input-group"><label for="BRLvalue">Payment expiry date</label>   <input type="text" name="dateOver" value="<?php echo $dateOver?>" disabled>           </div>
+            <div class="input-group">
+                <br>
+                <p class="login-text" style="font-size: 2rem; font-weight: 800;"><center><h3>Payee's Full Info</h3></center><br></p>
+            </div><br>
+            <?php printf("Name : <i>". $namerecipient. "</i>".
+                     "<br>Bank : <i>".$bank.              "</i>".
+                     "<br>Branch : <i>".$branch.          "</i>".
+                     "<br>Account : <i>".$account.         "</i>".
+                     "<br>Type : <i>".$type); ?>            </i>
+                     <br><br><br>
+			<div class="input-group"><button name="submit" class="btn">Create Invoice</button></div>
 			<p class="login-register-text"><a href="https://www.granna.ie/login/main.php">Cancel</a></p>
+
 		</form>
 	</div>
 </body>
